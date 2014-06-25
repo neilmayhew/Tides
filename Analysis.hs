13a68478868a9c66544630b91b87c46c52382846
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2014-06-25 11:36:28 -0600

    Apply hlint suggestions

--- Analysis.hs
+++ Analysis.hs
@@ -14,19 +14,21 @@ extremum :: (Fractional t, Ord t) =>
 extremum f f' f'' e r = Extremum x y t
   where x = solve f' f'' e r
         y = f x
-        t = if c > e then Minimum else if c < (-e) then Maximum else Inflection 
-          where c = f'' x
+        c = f'' x
+        t | c > e = Minimum
+          | c < (-e) = Maximum
+          | otherwise = Inflection
 
 -- TODO: use bisection to ensure bounds
 solve :: (Fractional t, Ord t) =>
      (t -> t) -> (t -> t) -> t -> (t, t) -> t
 solve f f' e (x0, x1) = head . convergedBy e . iterate step $ x0
-  where step x = x - (f x) / (f' x)
+  where step x = x - f x / f' x
 
 dropWhile2 :: (t -> t -> Bool) -> [t] -> [t]
 dropWhile2 p xs@(x : xs'@(x' : _)) = if not (p x x') then xs else dropWhile2 p xs'
 dropWhile2 _ any = any
 
 convergedBy :: (Num t, Ord t) => t -> [t] -> [t]
-convergedBy e seq = dropWhile2 unconverging seq
+convergedBy e = dropWhile2 unconverging
   where unconverging x x' = abs (x - x') >= e
