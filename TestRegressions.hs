8e7fe1fb517480790461339e2947377a76d4c605
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2014-06-25 22:25:13 -0600

    Move regression testing into cabal file

---
+++ TestRegressions.hs
@@ -0,0 +1,20 @@
+import Data.Monoid
+import Text.Printf
+import System.Cmd
+import System.Exit
+
+instance Monoid ExitCode where
+    mempty = ExitSuccess
+    mappend ExitSuccess b = b
+    mappend a           _ = a
+
+main = mapM test [ ("TestTCD"         , "Hinkley"           )
+                 , ("TideConstituents", "2014"              )
+                 , ("TideAmplitudes"  , "Hinkley 2014"      )
+                 , ("Tides"           , "Hinkley 2014-06-13")
+                 ]
+        >>= exitWith . mconcat
+
+test (prog, args) = do
+    putStrLn $ "==== " ++ prog ++ " ===="
+    system $ printf "dist/build/%s/%s %s 2>&1 | diff %s.out -" prog prog args prog
