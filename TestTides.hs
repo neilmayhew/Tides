c7f011e4093842cf0457eaa4523db83338206d04
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2014-07-13 19:20:17 -0600

    Implement Random instances for various Time types

--- TestTides.hs
+++ TestTides.hs
@@ -5,6 +5,7 @@ import Data.Time
 import HSH
 import System.Environment
 import System.Locale (defaultTimeLocale)
+import System.Random
 import Text.Printf
 
 main = do
@@ -30,3 +31,32 @@ getModel location begin end step =
     xtideTime     = formatTime defaultTimeLocale "%F %H:%M"
     xtideInterval = formatTime defaultTimeLocale "%H:%M" . timeToTimeOfDay . realToFrac
     parseLine = second read . head . readsTime defaultTimeLocale "%F %l:%M %p %Z"
+
+instance Random DiffTime where
+    random = first secondsToDiffTime . random
+    randomR (min, max) = first secondsToDiffTime . randomR (round min, round max)
+
+instance Random NominalDiffTime where
+    random = first timeToNominal . random
+    randomR (min, max) = first timeToNominal . randomR (nominalToTime min, nominalToTime max)
+
+instance Random TimeOfDay where
+    random = first timeToTimeOfDay . randomR (0, 86399)
+    randomR (min, max) = first timeToTimeOfDay . randomR (timeOfDayToTime min, timeOfDayToTime max)
+
+instance Random Day where
+    random = first ModifiedJulianDay . random
+    randomR (min, max) = first ModifiedJulianDay . randomR (toModifiedJulianDay min, toModifiedJulianDay max)
+
+instance Random LocalTime where
+    randomR (min, max) = first (utcToLocalTime utc) . randomR (localTimeToUTC utc min, localTimeToUTC utc max)
+    random = first (utcToLocalTime utc) . random
+
+instance Random UTCTime where
+    randomR (min, max) = first (`addUTCTime` min) . randomR (0, max `diffUTCTime` min)
+    random g = (UTCTime d t, g'')
+      where (d, g' ) = random g
+            (t, g'') = first timeOfDayToTime $ random g'
+
+timeToNominal = realToFrac :: DiffTime -> NominalDiffTime
+nominalToTime = realToFrac :: NominalDiffTime -> DiffTime
