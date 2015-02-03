609358e1c617680e7734ca8762eced3e17032439
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2015-02-02 17:01:29 -0700

    Refactor Tides into a main program and reusable module

--- Tides.hs
+++ Tides.hs
@@ -1,45 +1,16 @@
+module Tides where
+
 import TCD
 import Harmonics
 import Analysis
 import Time
 
 import Control.Arrow (first)
-import Control.Monad
+import Control.Monad (unless, (<=<))
 import Data.Function
 import Data.List
 import Data.Time
 import Data.Time.Zones
-import System.Environment
-import System.Exit
-import System.Locale (defaultTimeLocale)
-import Text.Printf
-
-main = do
-    [location, begin, end, step] <- getArgs
-
-    let toTime     = readTime defaultTimeLocale "%F %H:%M"
-        toInterval = realToFrac . timeOfDayToTime . readTime defaultTimeLocale "%H:%M"
-
-    (heights, events, units) <- tides location  (toTime begin) (toTime end) (toInterval step)
-
-    -- Output tide heights for the period
-
-    forM_ heights $ \(t, h) ->
-        putStrLn $ printf "%s %10.6f" (showTime t) h
-
-    -- Output low and high tides for the period
-
-    forM_ events $ \(Extremum (t, h) c) ->
-        putStrLn $ printf "%s %6.2f %s  %s Tide"
-            (showTime t) h units (showType c)
-  where
-    showTime :: ZonedTime -> String
-    showTime t = formatTime defaultTimeLocale "%F %l:%M %p %Z" t'
-      where t' = addZonedTime 30 t -- Display nearest minute
-    addZonedTime s t = utcToZonedTime (zonedTimeZone t) (s `addUTCTime` zonedTimeToUTC t)
-    showType Maximum    = "High"
-    showType Minimum    = "Low"
-    showType Inflection = "Stationary" -- Never happens?
 
 type Prediction = (ZonedTime, Double)
 
