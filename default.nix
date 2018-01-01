93bddf019e2e1c239d657a30b63e9d285b96d9b9
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2018-01-01 13:44:42 -0700

    Add harmonics and xtide to shell environment, for TestTides

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@
 let
   tcd       = callPackage ./libtcd.nix    {};
   harmonics = callPackage ./harmonics.nix { type = harmonicsType; };
+  xtide     = callPackage ./xtide.nix     {};
 
   inherit (stdenv.lib) concatStringsSep;
 in
@@ -27,6 +28,8 @@ in
       configure() {
         cabal configure --enable-tests '${concatStringsSep "' '" configureFlags}' "$@"
       }
+      export HFILE_PATH=${harmonics}/share/xtide
+      export PATH=${xtide}/bin:$PATH
     '';
     preCheck = "export TZDIR=${tzdata}/share/zoneinfo";
     description = "A program for exploring tidal prediction data";
