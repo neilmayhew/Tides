35db4088bcf5b9740dd58922f4cede36599315a7
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2022-05-28 08:43:19 -0600

    harmonics: Use a locally-cached copy of nonfree data
    The online copy has been removed

--- harmonics.nix
+++ harmonics.nix
@@ -3,22 +3,22 @@
 let
   free = rec {
     version = "20190620";
-    url = "https://flaterco.com/files/xtide/harmonics-dwf-${version}-free.tar.bz2";
-    sha256 = "05pn97ls3igq3jjyy7kjcwzh937kl47sbk1b1xhjkhb10zrld5c7";
+    src = fetchurl {
+      url = "https://flaterco.com/files/xtide/harmonics-dwf-${version}-free.tar.bz2";
+      sha256 = "05pn97ls3igq3jjyy7kjcwzh937kl47sbk1b1xhjkhb10zrld5c7";
+    };
     sourceRoot = "";
   };
   nonfree = rec {
     version = "20111230";
-    url = "ftp://ftp.flaterco.com/xtide/harmonics-dwf-${version}-nonfree.tar.bz2";
-    sha256 = "0759dk9rdr5rklz3xg2riy02afj7s239w61irmjgvhd4psnx4k6l";
+    src = Downloads/ftp.flaterco.com/xtide/harmonics-dwf-${version}-nonfree.tar.bz2;
     sourceRoot = ".";
   };
   origin = if type == "free" then free else nonfree;
 in
   stdenv.mkDerivation {
     name = "xtide-data-${type}";
-    inherit (origin) version sourceRoot;
-    src = fetchurl { inherit (origin) url sha256; };
+    inherit (origin) version src sourceRoot;
     installPhase = ''
       shopt -s extglob
       mkdir -p $out/share/xtide/
