38be41a1c00dcb7cc54d9ca4e7f51dc4abfae203
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2023-01-20 23:24:48 -0700

    Update xtide to 2.15.5 in nix derivation

--- xtide.nix
+++ xtide.nix
@@ -9,10 +9,10 @@ let
 in
   stdenv.mkDerivation rec {
     pname = "xtide";
-    version = "2.15.2";
+    version = "2.15.5";
     src = fetchurl {
-      url = "https://flaterco.com/files/xtide/xtide-${version}.tar.bz2";
-      sha256 = "0cb9lhzs2asqlp0m505qgzlykwjlcq2rf81kk3kvcmdns67jw768";
+      url = "https://flaterco.com/files/xtide/xtide-${version}.tar.xz";
+      sha256 = "0r3i1mqz59awpflcdssa7zy9fjni2qapajmkvi29b3by2v7hsimr";
     };
     buildInputs = [ zlib libpng tcd libX11 libXext libXpm libXaw Xaw3d ];
     meta = {
