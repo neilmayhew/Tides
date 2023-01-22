1e66ac59ab00376a026606e6beee74f0274d9207
Neil Mayhew <neil_mayhew@users.sourceforge.net>
2023-01-22 16:36:13 -0700

    Update libtcd to 2.2.7-r3 in nix derivation

--- libtcd.nix
+++ libtcd.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "libtcd";
-  version = "2.2.7-r2";
+  version = "2.2.7-r3";
   src = fetchurl {
-    url = "https://flaterco.com/files/xtide/libtcd-${version}.tar.bz2";
-    sha256 = "150a099n4bhfrk6n2mnv2ai1x3l23k8jr4cls1rca1j1p0cg5wdg";
+    url = "https://flaterco.com/files/xtide/libtcd-${version}.tar.xz";
+    sha256 = "0xyqlal5f0625dw743hak406h76ng55rpabnqrbsn7kpzfmfkpg1";
   };
   meta = {
     description = "A software API for reading and writing Tide Constituent Database (TCD) files";
